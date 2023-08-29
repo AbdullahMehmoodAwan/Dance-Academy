9194357f7b223ea84ced1ca80c713c701123c74f
Abdullah Mehmood <abdullah67sp@gmail.com>
2023-08-30 01:20:24 +0500

    V 2.0

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 const app = express();
@@ -12,6 +13,22 @@ app.use(express.urlencoded())
 app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
 
+
+//Mongoose database
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');}
+
+  const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+  const Contact = mongoose.model('Contact', contactSchema);
+
 // ENDPOINTS
 app.get('/', (req, res)=>{
     const params = {}
@@ -21,7 +38,15 @@ app.get('/contact', (req, res)=>{
     const params = {}
     res.status(200).render('contact.pug', params);
 })
-
+app.post('/contact', (req, res)=>{
+    const myData = new Contact(req.body);
+    myData.save().then(()=> {
+        res.send("This item has been saved tp the database")
+    }).catch(()=>{
+        res.status(400).send("Items was not saved to database")
+    });
+});
+ 
 
 
 
